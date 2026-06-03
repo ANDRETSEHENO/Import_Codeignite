8848e44d9479d130d06e2b44818956e1d718a513
christon <andretsehenoc@gmail.com>
2026-06-03 17:32:22 +0300

    import-csv- avec validation

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,5 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'ImportController::index');
-$routes->get('/import', 'ImportController::index');
-$routes->post('/import/upload', 'ImportController::upload');
+$routes->post('upload', 'ImportController::upload');
